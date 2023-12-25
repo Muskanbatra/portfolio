58c36eb744ad447d6abbcb38d1305b41fac80035
durgeshUnico <durgesh.unicoglobal@gmail.com>
2023-12-25 11:31:27 +0530

    height correct

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -5,7 +5,7 @@ import { SlArrowRightCircle } from "react-icons/sl";
 export const Project = () => {
     return (
         <>
-            <div class="container ml-32 mt-24">
+            <div class="container pl-32 mt-24 w-full absolute">
                 <img src={p1} alt="Your Image" class="image ml-6"></img>
 
                 <div className='z-20'>
@@ -13,9 +13,8 @@ export const Project = () => {
                     <p class="  text-overlay1 text-2xl text-sky-400 ">~ Educational website</p>
                     <a href="" class=" flex gap-4 text-overlay2 text-3xl font-thin  text-white ">View work <SlArrowRightCircle /></a>
                 </div>
+                <div className='bgcolorp'></div>
             </div>
-
-            <div className='bgcolorp'></div>
         </>
     )
 }
