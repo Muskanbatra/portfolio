08424fc8d18194c9c1b81795c9ef2baadcff09e2
durgesh730 <durgesh.unicoglobal@gmail.com>
2024-04-28 22:57:01 +0530

    checked

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,12 @@
+import './Home.css'
 import React from 'react'
+import p from '../../Images/photo.png'
 import { Navbar } from '../Navbar/Navbar'
 import { Footer } from '../Footer/Footer'
-import p from '../../Images/photo.png'
-import p1 from '../../Images/p1.png'
-import './Home.css'
-import { SlArrowRightCircle } from "react-icons/sl";
 import { Project } from '../Project/Project'
 import { Contact } from '../Contact/Contact'
 import { Contact1 } from '../Contact1/Contact1'
+
 export const Home = () => {
   return (
     <>
@@ -18,7 +17,6 @@ export const Home = () => {
           <img src={p} alt="image description"></img>
           <div className='bgcolor'></div>
         </div>
-
 
         <div className='hometext pt-24'>
           <div class="text-9xl text-teal-400 font-bold"><h1>Hello</h1></div>
@@ -46,13 +44,13 @@ export const Home = () => {
           </div>
         </div>
       </div>
+
       <div className='experience ml-24'>
         <div class="text-6xl text-teal-400 font-bold"><h1>#2</h1></div>
         <div class="text-5xl text-sky-400 font-medium pt-3"><h1>Experience</h1></div>
       </div>
 
       <div class="flex ">
-
         <div id="card" class="p-8 mt-28 mr-7 ml-28  cursor-pointer  bg-gray-100 transition duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl bg-custom-color">
           <p class=" bg-custom-color text-3xl text-teal-400 font-bold pt-5">2019</p>
           <p class=" bg-custom-color text-2xl text-sky-400 font-medium pt-5">Front-End Developer</p>
@@ -100,15 +98,9 @@ export const Home = () => {
         <div class="text-6xl text-teal-400 font-bold"><h1>#3</h1></div>
         <div class="text-5xl text-sky-400 font-medium pt-3"><h1>Projects</h1></div>
       </div>
-
-     
-      <Project/>
-      <Contact1/>
-      <Footer/>
-     
-
+      <Project />
+      <Contact1 />
+      <Footer />
     </>
-
-
   )
 }
